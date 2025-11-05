5638460ba11e8fdd6116feeec9d28565b39b2984
Bianca Sarahi Fernandez Quispe <70457953+BiancaFernandezQ@users.noreply.github.com>
2025-11-05 19:15:35 -0400

    arreglo config y logger

--- src/helpers/customer_helper.py
+++ src/helpers/customer_helper.py
@@ -3,6 +3,9 @@ from src.services.customer_service import CustomerService
 Faker.locale = 'es_ES'
 import json
 import time
+from src.utils.logger import setup_logger
+
+logger = setup_logger('customer_helper')
 
 class CustomerHelper:
 
@@ -15,6 +18,7 @@ class CustomerHelper:
             "gender" : Faker().random_element(['Male', 'Female', 'Other']),
             "customer_group_id" : 1,
         }
+        logger.info(f"Creando cliente aleatorio con email: {customer_data['email']}")
         return CustomerService.create_customer(token, customer_data)
     
     @staticmethod
@@ -46,4 +50,6 @@ class CustomerHelper:
         if phone is not None:
             #customer_data["phone"] = phone or Faker().phone_number()
             customer_data["phone"] = phone or Faker().random_number(digits=10, fix_len=True)
+        
+        logger.info(f"Construyendo cliente aleatorio con email: {customer_data['email']}")
         return customer_data
--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,12 +2,12 @@ import logging
 import os
 from logging.handlers import RotatingFileHandler
 
-#ver menos detalles de logger level debe ser igual a 
-def setup_logger(name, log_file='logs/combined.log', level=logging.WARNING):
+def setup_logger(name, log_file='logs/combinado.log', level=logging.INFO):
+    
     os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(name)s - error en: %(funcName)s:%(lineno)d - %(message)s')
+    
     logger = logging.getLogger(name)
     if not logger.handlers:
         file_handler = RotatingFileHandler(log_file, maxBytes=5 * 1024 * 1024, backupCount=2)
@@ -21,7 +21,6 @@ def setup_logger(name, log_file='logs/combined.log', level=logging.WARNING):
     logger.setLevel(level)
 
     return logger
-
 
 if __name__ == "__main__":
     logger = setup_logger('bagisto_API')
